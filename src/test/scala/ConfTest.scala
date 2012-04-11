c1d88a7d772c066494fbdc5ce626bcdc6ee1391f
Rogach <platon7pronko@gmail.com>
2012-04-11 17:45:06 +0400

    added test for toggling boolean option

--- src/test/scala/ConfTest.scala
+++ src/test/scala/ConfTest.scala
@@ -126,4 +126,14 @@ class ConfTest extends FunSuite with ShouldMatchers {
     Conf.apples.isSupplied should equal (false)
   }
   
+  test ("toggle flag option") {
+    object Conf extends ScallopConf(List("-a")) {
+      val apples = opt[Boolean]("apples").map(!_)
+      val bananas = opt[Boolean]("bananas").map(!_)
+      verify
+    }
+    Conf.apples() should equal (false)
+    Conf.bananas() should equal (true)
+  }
+  
 }
