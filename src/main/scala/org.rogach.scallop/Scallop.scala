af082e8724b07c2a14d66040b05ecedaa18ddbd3
Rogach <platon7pronko@gmail.com>
2012-05-26 15:59:53 +0400

    added support for querying option invocations in value converters

--- src/main/scala/org.rogach.scallop/Scallop.scala
+++ src/main/scala/org.rogach.scallop/Scallop.scala
@@ -35,13 +35,13 @@ case class Scallop(
     optionSetValidations: List[List[String]=>Either[String, Unit]] = Nil) {
 
   /** Parse the argument into list of options and their arguments. */
-  private def parse(args: Seq[String]): List[(CliOption, List[String])] = {
-    def goParseRest(args: Seq[String], opt: Option[CliOption]) = { 
+  private def parse(args: Seq[String]): List[(CliOption, (String,List[String]))] = {
+    def goParseRest(args: Seq[String], opt: Option[(String, CliOption)]) = { 
       parseTrailingArgs(
         args.toList,
-        opt.map(o=> (o.converter, true)).toList ::: opts.filter(_.isPositional).map(o => (o.converter, o.required))
-      ) map { res => (opt.toList ::: opts.filter(_.isPositional)) zip res filter { case (opt, p) => !opt.isPositional || p.size > 0 } } getOrElse
-        (throw new OptionParseException("Failed to parse the trailing argument list: '%s'" format args.tail))
+        opt.map(o=> (o._2.converter, true)).toList ::: opts.filter(_.isPositional).map(o => (o.converter, o.required))
+      ) map { res => (opt.toList ::: opts.filter(_.isPositional).map(("",_))) zip res filter { case ((invoc, opt), p) => !opt.isPositional || p.size > 0 } } getOrElse
+        (throw new OptionParseException("Failed to parse the trailing argument list: '%s'" format args.tail)) map { case ((invoc, opt), p) => (opt, (invoc, p)) }
     }
     if (args.isEmpty) Nil
     else if (isOptionName(args.head)) {
@@ -51,9 +51,9 @@ case class Scallop(
         val (before, after) = args.tail.span(isArgument)
         if (after.isEmpty) {
           // get the converter, proceed to trailing args parsing
-          goParseRest(args.tail, Some(opt))
+          goParseRest(args.tail, Some((args.head.drop(2),opt)))
         } else {
-          (opt -> before.toList) :: parse(after)
+          (opt -> (args.head.drop(2), before.toList)) :: parse(after)
         }
       } else {
         if (args.head.size == 2) {
@@ -62,9 +62,9 @@ case class Scallop(
           val (before, after) = args.tail.span(isArgument)
           if (after.isEmpty) {
             // get the converter, proceed to trailing args parsing
-            goParseRest(args.tail, Some(opt))
+            goParseRest(args.tail, Some((args.head.drop(1), opt)))
           } else {
-            (opt -> before.toList) :: parse(after)
+            (opt -> (args.head.drop(1), before.toList)) :: parse(after)
           }
         } else {
           val opt = getOptionWithShortName(args.head(1)) getOrElse
@@ -94,7 +94,7 @@ case class Scallop(
   }
   
   /** Result of parsing */ 
-  private lazy val parsed = if (args.headOption map("@--" ==) getOrElse false) {
+  private lazy val parsed: List[(CliOption, (String, List[String]))] = if (args.headOption map("@--" ==) getOrElse false) {
     // read options from stdin
     val argList = 
       io.Source.fromInputStream(java.lang.System.in).getLines.toList
@@ -154,7 +154,7 @@ case class Scallop(
             if (next.isDefined) Some(p :: next.get)
             else None 
         } else {
-          convs.head._1.parse(List(p)) match {
+          convs.head._1.parse(List(("",p))) match {
             case Right(a) if a.isDefined => 
               val next = parseTrailingArgs(rem, convs.tail)
               if (next.isDefined) Some(p :: next.get)
@@ -398,7 +398,7 @@ case class Scallop(
       val args = parsed filter (_._1 == o) map (_._2)
       val res = o.converter.parse(args)
       if (res.isLeft) throw new WrongOptionFormat(
-        "Wrong format for option '%s': %s" format (o.name, args.map(_.mkString).mkString(" ")))
+        "Wrong format for option '%s': %s" format (o.name, args.map(_._2.mkString).mkString(" ")))
       if (o.required && !res.right.get.isDefined && !o.default.isDefined) throw new RequiredOptionNotFound(
         "Required option '%s' not found" format o.name)
       // validaiton
