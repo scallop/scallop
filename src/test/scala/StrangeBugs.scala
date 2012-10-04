b5b7626b43d0290ef3d9cc09d86aa1127b892348
Rogach <platon7pronko@gmail.com>
2012-10-05 02:17:14 +0400

    Added test case

---
+++ src/test/scala/StrangeBugs.scala
@@ -0,0 +1,23 @@
+package org.rogach.scallop
+
+import org.rogach.scallop._
+import org.rogach.scallop.exceptions._
+
+class StrangeBugs extends UsefulMatchers {
+  throwError.value = true
+  
+  test ("funny bug with option name guessing") {
+    object Conf extends ScallopConf(Seq("-b")) {
+      val nonGuessedName = opt[Boolean]("bananas")
+    }
+    Conf
+
+    intercept[IdenticalOptionNames] {
+      val conf = new ScallopConf() {
+        val guessedName = opt[Int]()
+        val nonGuessedName = opt[Int]()
+      }
+    }
+  }
+  
+}
