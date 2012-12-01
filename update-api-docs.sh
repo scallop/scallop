564480be574652a1453f6009bb6d9ff6da5fa722
Rogach <platon7pronko@gmail.com>
2012-12-01 15:24:13 +0400

    fixed update-api util

--- update-api-docs.sh
+++ update-api-docs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sbt doc
-for x in $(find target/scala-2.9.1/api/ -type f); do sed -i "s_`pwd`/__" $x; done
+for x in $(find target/scala-2.9.2/api/ -type f); do sed -i "s_`pwd`/__" $x; done
 git checkout gh-pages
 rm -r index
 rm -r org
-cp -r target/scala-2.9.1/api/* ./
+cp -r target/scala-2.9.2/api/* ./
 git add .
 git commit -m "updated api docs"
 git push
