8b4daad6f553f1997475a02f6399a840290f84b4
Rogach <platon7pronko@gmail.com>
2013-01-11 20:28:31 +0400

    Moved helpers for StrangeTest into separate trait

--- src/test/scala/StrangeTest.scala
+++ src/test/scala/StrangeTest.scala
@@ -1,68 +1,9 @@
 package org.rogach.scallop
 
-import org.rogach.scallop._
 import org.rogach.scallop.exceptions._
-import java.io.ByteArrayOutputStream
-import java.io.ByteArrayInputStream
-import java.lang.{System, SecurityManager, SecurityException}
-import java.security.Permission
 
-class StrangeTest extends UsefulMatchers {
+class StrangeTest extends UsefulMatchers with CapturingTest {
   throwError.value = false
-  
-  /** Captures all output from the *fn* block into two strings - (stdout, stderr). */
-  def captureOutput(fn: => Unit):(String,String) = {
-    val normalOut = Console.out
-    val normalErr = Console.err
-    val streamOut = new ByteArrayOutputStream()
-    val streamErr = new ByteArrayOutputStream()
-    Console.setOut(streamOut)
-    Console.setErr(streamErr)
-    fn
-    Console.setOut(normalOut)
-    Console.setErr(normalErr)
-    (streamOut.toString, streamErr.toString)
-  }
-  
-  /** Supresses exit in *fn* block. Returns list of exit statuses that were attempted. */
-  def trapExit(fn: => Unit):List[Int] = {
-    @volatile var statuses = List[Int]()
-    val normalSM = System.getSecurityManager
-    object SM extends SecurityManager {
-      override def checkExit(status:Int) {
-        statuses ::= status
-        throw new SecurityException
-      }
-      override def checkPermission(p:Permission) {}
-    }
-    System.setSecurityManager(SM)
-    try {
-      fn
-    } catch { 
-      case e:SecurityException => 
-    }
-    System.setSecurityManager(normalSM)
-    statuses.reverse
-  }
-  
-  /** Supresses exits in *fn* block, and captures stdout/stderr. */
-  def captureOutputAndExits(fn: => Unit): (String, String, List[Int]) = {
-    var exits = List[Int]()
-    val (out, err) = captureOutput {
-      exits = trapExit(fn)
-    }
-    (out, err, exits)
-  }
-
-  /** Runs program with needed input. */
-  def withInput[A](input:String)(fn: => A):A = {
-    val normalIn = System.in
-    val streamIn = new ByteArrayInputStream(input.getBytes)
-    System.setIn(streamIn)
-    val res = fn
-    System.setIn(normalIn)
-    res
-  }
   
   test("help printing") {
     val (out, err) = captureOutput {
